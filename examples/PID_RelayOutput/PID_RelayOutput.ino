1e2ebf9e074a23bfd6050b61d7d7368b6ef70b30
olehs <olehs@i.ua>
2015-07-25 14:26:11 +0300

    Update PID_RelayOutput.ino
    Revert

--- examples/PID_RelayOutput/PID_RelayOutput.ino
+++ examples/PID_RelayOutput/PID_RelayOutput.ino
@@ -48,6 +48,7 @@ void setup()
 void loop()
 {
   Input = analogRead(PIN_INPUT);
+  myPID.Compute();
 
   /************************************************
    * turn the output pin on/off based on pid output
@@ -55,7 +56,6 @@ void loop()
   if (millis() - windowStartTime > WindowSize)
   { //time to shift the Relay Window
     windowStartTime += WindowSize;
-    myPID.Compute();
   }
   if (Output > millis() - windowStartTime) digitalWrite(RELAY_PIN, HIGH);
   else digitalWrite(RELAY_PIN, LOW);
